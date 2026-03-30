96510027c3b48c4547f97f50c21c530deefe1201
elmaaroufi said <saidoxcasa17@gmail.com>
2026-03-30 23:52:58 +0100

    we try to fix the form bugs after store a product

--- src/components/ADD/addForm.jsx
+++ src/components/ADD/addForm.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+const AddForm = ({products, setproducts}) => {
+    const [error, setError] = useState('')
+    const [formData, setFormdata] = useState(
+        {
+            nomFr : '', 
+            nomAr:'', 
+            marque:'', 
+            code:'',
+            brCode:'',
+            img:'',
+            prix:'',
+            Qte:''
+        }
+    )
 
-const AddForm = () => {
+    const handelValue = (e) => {
+        let nom = e.target.name
+        setFormdata({...formData, [nom] : e.target.value})
+    }
+
+    const add = () => {
+        console.log('im herre')
+        let res = Object.values(formData).some((value) => value.trim() === '')
+        if(res === true ){
+            setError("<p className='alert alert-danger'>il ya un champ vide</p>")
+        } else{
+            setError("<p className='alert alert-success'>le produit a ete ajouter avec succeess</p>")
+            setproducts([...products, formData])
+        }
+        setError('')
+    }
+
+
     return ( 
 
         /*
@@ -17,42 +49,43 @@ const AddForm = () => {
         <div className="container mt-3">
             <form action="" className="border m-3 p-2">
                 <h2 className="text-center bg-secondary p-2 text-light">Ajouter un Produit</h2>
+                {error||<p className="alert alert-danger">hi</p>}
                 <div className="container">
 
                 <div className="my-4 ">
-                    <label htmlFor="" className="form-label">Nom en Français:</label>
-                    <input type="text" className="form-control" />
+                    <label htmlFor=""  className="form-label">Nom en Français:</label>
+                    <input type="text" name="nomFr" value={formData.nomFr} onChange={handelValue} className="form-control" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="" className="form-label">Nom en Arabe:</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" name="nomAr" value={formData.nomAr} onChange={handelValue} className="form-control" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="" className="form-label">Marque:</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" name="marque" value={formData.marque} onChange={handelValue} className="form-control" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="" className="form-label">code:</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" name="code" value={formData.code} onChange={handelValue} className="form-control" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="" className="form-label">barre code:</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" name="brCode" value={formData.brCode} onChange={handelValue} className="form-control" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="" className="form-label">Image:</label>
-                    <input type="file" className="form-control" />
+                    <input type="file" name="image" value={formData.img} onChange={handelValue} className="form-control" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="" className="form-label">Prix:</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" name="prix" value={formData.prix} onChange={handelValue} className="form-control" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="" className="form-label">Qte:</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" name="Qte" value={formData.Qte} onChange={handelValue} className="form-control" />
                 </div>
                 <div className="d-grid">
-                    <button className=" btn btn-outline-success ">Ajouter</button>
+                    <button type="button" className=" btn btn-outline-success " onClick={add}>Ajouter</button>
                 </div>
                 </div>
             </form>
--- src/components/ADD/addMethods.jsx
+++ src/components/ADD/addMethods.jsx
@@ -3,8 +3,8 @@ const AddMethods = () => {
     return ( 
         <div className="container mt-3">
             <div className="d-flex justify-content-center align-items-center vh-100 gap-3">
-                    <Link to="/imageField " className="btn btn-primary">with Image</Link>
-                    <Link to="/formField" className="btn btn-success">with Form</Link>
+                    <Link to="/imageField " className="btn btn-primary w-25 h-25 ">with Image</Link>
+                    <Link to="/formField" className="btn btn-success w-25 h-25">with Form</Link>
                 
             </div>
         </div>
